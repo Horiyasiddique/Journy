23e80fd0a1331a1946c1383bcdf545d8a45e9043
Farah-ace <farahserver123@gmail.com>
2025-09-27 21:25:18 +0500

    No changes

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,7 +1,13 @@
 import { Outlet } from 'react-router'
 import Header from './components/Header/Header'
 
+<<<<<<< Updated upstream:src/MainLayout.jsx
 const MainLayout = () => {
+=======
+
+const Layout = () => {
+ 
+>>>>>>> Stashed changes:src/Layout.jsx
   return (
     <div>
       <Header />
